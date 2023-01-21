53551d374ecfc6c5535efe46e213d8902e7405d3
jhon wick <charliechaplin194@outlook.com>
2023-01-22 00:07:15 +0530

    second commit

--- sakphal.py
+++ sakphal.py
@@ -1,3 +1,5 @@
-first_name="Srijan Sakphal"
-last_name="T"
+# first_name="Srijan Sakphal"
+# last_name="T"
+first_name="Pakkirappa"
+last_name="Uppara"
 print(f"My name is {last_name} {first_name}")
